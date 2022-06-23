8c3e6eda1a6a298e560e48bd508fe1d54492fff6
Lucas Mezencio <lucasmezencio@outlook.com>
2022-06-23 13:07:13 -0300

    create docs to default handler

--- handlers/defaultHandler.go
+++ handlers/defaultHandler.go
@@ -5,6 +5,9 @@ import (
 	"net/http"
 )
 
+// NoRouteHandler handlers the default and the root (/) route
+//
+// Returns a NotFound Status
 func NoRouteHandler(c *gin.Context) {
 	c.JSON(http.StatusNotFound, nil)
 }
