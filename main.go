27a6c7332eca2277e08031893c5f0aacdb317cf4
Lucas Mezencio <lucasmezencio@outlook.com>
2022-06-23 11:21:22 -0300

    change main to server

--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"redis-task/database/scripts"
-	"redis-task/models"
+	"redis-task/routes"
 )
 
 func main() {
 	scripts.FlushDatabase()
 	scripts.PopulateDatabase(nil)
-	err := models.DeleteBlockById("F3")
-	if err != nil {
-		fmt.Println(err)
-	}
-	//routes.HandleRoutes()
+	routes.HandleRoutes()
 }
