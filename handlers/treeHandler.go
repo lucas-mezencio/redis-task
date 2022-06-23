2a2455e775662ecee32274b25b248e3e1b9402f3
Lucas Mezencio <lucasmezencio@outlook.com>
2022-06-23 13:11:46 -0300

    create docs to tree handler

--- handlers/treeHandler.go
+++ handlers/treeHandler.go
@@ -7,6 +7,11 @@ import (
 	"reflect"
 )
 
+// GetTreeBellowId handles the route to get a tree based on a given id
+//			GET /tree/id
+// If the root block doesn't exist return a NotFound Status
+//
+// Returns the tree with the given id block as root and OK Status
 func GetTreeBellowId(c *gin.Context) {
 	id := c.Param("id")
 	tree := models.GetTreeById(id)
